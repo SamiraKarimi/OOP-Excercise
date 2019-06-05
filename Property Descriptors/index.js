5b030038b135fbe0063c91d00155739c9374ff8b
Samira Karimi <samira.ka006@gmail.com>
2019-06-05 13:04:06 -0500

    defineProperty

--- Property Descriptors/index.js
+++ Property Descriptors/index.js
@@ -1,5 +1,14 @@
 let person = { name: "Sammy" };
 
-let objectBase = Object.getPrototypeOf(person);
-let descriptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
-console.log(descriptor);
+// let objectBase = Object.getPrototypeOf(person);
+// let descriptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
+// console.log(descriptor);
+
+Object.defineProperty(person, "name", {
+  /*by defualt all of these attribute are true*/
+  writable: false,
+  enumerable: false,
+  configurable: false
+});
+person.name = "John";
+console.log(Object.keys(person));
